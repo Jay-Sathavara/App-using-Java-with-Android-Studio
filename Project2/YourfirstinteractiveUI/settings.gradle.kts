fe2e7812addfc9df90970687f61397b79315736b
JP0030 <thekeen003@gmail.com>
2024-01-31 09:23:16 +0530

    by this we start to learn Android Devlopment using Java and Android Studio

---
+++ Project2/YourfirstinteractiveUI/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Your first interactive UI"
+include(":app")
+ 
